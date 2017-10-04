3790f0557cbc160838823365011f876b220b8848
v-chigur <v-chigur@mail.ru>
2017-10-04 20:46:12 +0300

    stress testing

--- gen.py
+++ gen.py
@@ -6,5 +6,7 @@ def num():
     return str(randrange(10))
 def op():
     return choice(['-', '+', '*', '/', '%', '^'])
+def f():
+    return choice([choice(['sin', 'cos', 'tan', 'sqrt', 'asin', 'acos', 'atan']) + '(' + num() + ')'] + ['log' + '(' + num() + ',' + num() + ')'] )
 
-print(num() + op() + '(' + num() + op() + num() + ')' + op() + num())
+print(num() + op() + '(' + num() + op() + f() + op() + num() + ')' + op() + num())
